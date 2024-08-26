87650cf0679bb5c0554501183eade5c3a56ffe59
Abdel-Rahman Nasser <96590314+AbdelRahmanNasser20@users.noreply.github.com>
2024-08-25 21:13:34 -0700

    init commit

--- backend/run.py
+++ backend/run.py
@@ -5,5 +5,5 @@ app = create_app()
 if __name__ == '__main__':
     port = int(os.getenv('BACKEND_PORT', 5001))
     debug_mode = os.getenv('DEBUG', 'True') == 'True'
-    print("BACKEND_PORT =", port)    
+    print("BACKEND_PORT =", port)        
     app.run(host='0.0.0.0', port=port, debug=debug_mode)
